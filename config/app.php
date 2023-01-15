a41ef9edeb1bd055c135f75ae9815e18341942bd
janilsonjvs <janilsonjvs@gmail.com>
2023-01-15 17:46:20 -0300

    feat: paginate defautl 10

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'paginate' => 10,
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
