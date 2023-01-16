a7faa9b8882c29c843d5f7a99115a0d3c6d4a530
janilsonjvs <janilsonjvs@gmail.com>
2023-01-16 15:09:47 -0300

    feat: model cliente

---
+++ app/Models/Cliente.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'nome',
+        'nome_fantasia',
+        'cpfcnpj',
+        'logradouro',
+        'numero',
+        'cep',
+        'complemento',
+        'bairro',
+        'estado_id',
+        'cidade_id',
+        'fone',
+        'celular',
+        'email',
+        'ativo'
+    ];
+}
