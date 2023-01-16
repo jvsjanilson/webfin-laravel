446af42928ece731da88f689bf6be17b6c9b63d8
janilsonjvs <janilsonjvs@gmail.com>
2023-01-16 20:21:57 -0300

    feat: controller fornecedor

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\ExceptionErrorCreate;
+use App\Exceptions\ExceptionErrorDestroy;
+use App\Exceptions\ExceptionErrorUpdate;
+use App\Exceptions\ExceptionNotFound;
+use App\Http\Requests\FornecedorFormRequest;
+use App\Http\Resources\FornecedorCollection;
+use App\Http\Resources\FornecedorResource;
+use App\Models\Fornecedor;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class FornecedorController extends Controller
+{
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $regs = Fornecedor::paginate(config('app.paginate'));
+        return new FornecedorCollection($regs);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(FornecedorFormRequest $request)
+    {
+        $data = $request->all();
+        try {
+            Fornecedor::create($data);
+            return response('', Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            throw new ExceptionErrorCreate();
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $reg = Fornecedor::find($id);
+        if (!isset($reg))
+            throw new ExceptionNotFound();
+
+        return response()->json(new FornecedorResource($reg));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $reg = Fornecedor::find($id);
+        $data = $request->all();
+
+        if (!isset($reg)) {
+            throw new ExceptionNotFound();
+        }
+
+        try {
+            $reg->update($data);
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            throw new ExceptionErrorUpdate();
+
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            Fornecedor::find($id)->delete();
+            return response(null, Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            throw new ExceptionErrorDestroy();
+        }
+    }
+}
