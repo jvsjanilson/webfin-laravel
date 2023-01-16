da3271be538453af4c56b0c795f53b512f00b65d
janilsonjvs <janilsonjvs@gmail.com>
2023-01-16 15:27:34 -0300

    feat: controller Cliente

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\ExceptionErrorCreate;
+use App\Exceptions\ExceptionErrorDestroy;
+use App\Exceptions\ExceptionErrorUpdate;
+use App\Exceptions\ExceptionNotFound;
+use App\Http\Resources\ClienteCollection;
+use App\Http\Resources\ClienteResource;
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $regs = Cliente::paginate(config('app.paginate'));
+        return new ClienteCollection($regs);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        try {
+            Cliente::create($data);
+            return response('', Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            throw new ExceptionErrorCreate();
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $reg = Cliente::find($id);
+        return response()->json(new ClienteResource($reg));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $reg = Cliente::find($id);
+        $data = $request->all();
+
+        if (!isset($reg)) {
+            throw new ExceptionNotFound();
+        }
+
+        try {
+            $reg->update($data);
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            throw new ExceptionErrorUpdate();
+
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            Cliente::find($id)->delete();
+            return response(null, Response::HTTP_NO_CONTENT);
+        } catch (\Throwable $th) {
+            throw new ExceptionErrorDestroy();
+        }
+    }
+}
