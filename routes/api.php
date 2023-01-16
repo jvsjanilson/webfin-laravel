4286b965d8fefc4f0ee5b58c320d540a8d3a72cc
janilsonjvs <janilsonjvs@gmail.com>
2023-01-16 10:17:36 -0300

    feat: add rota para login e logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CidadeController;
 use App\Http\Controllers\EstadoController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout']);
+
 
 
 Route::apiResource('/estados', EstadoController::class);
