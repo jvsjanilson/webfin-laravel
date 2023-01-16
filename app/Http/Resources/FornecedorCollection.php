b97e678dff9c837bf7bc2183d023f25bedcb44b5
janilsonjvs <janilsonjvs@gmail.com>
2023-01-16 20:42:34 -0300

    feat: resource fornecedor

--- app/Http/Resources/FornecedorCollection.php
+++ app/Http/Resources/FornecedorCollection.php
@@ -14,6 +14,8 @@ class FornecedorCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+        ];
     }
 }
--- app/Http/Resources/FornecedorResource.php
+++ app/Http/Resources/FornecedorResource.php
@@ -14,6 +14,22 @@ class FornecedorResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'nome_fantasia' => $this->nome_fantasia,
+            'cpfcnpj' => $this->cpfcnpj,
+            'logradouro' => $this->logradouro,
+            'numero' => $this->numero,
+            'cep' => $this->cep,
+            'complemento' => $this->complemento,
+            'bairro' => $this->bairro,
+            'estado_id' => $this->estado_id,
+            'cidade_id' => $this->cidade_id,
+            'fone' => $this->fone,
+            'celular' => $this->celular,
+            'email' => $this->email,
+            'ativo' => $this->ativo,
+        ];
     }
 }
