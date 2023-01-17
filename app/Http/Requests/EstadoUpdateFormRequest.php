8e9a64f6cf91e4a65a56dd41e104c859d731c583
janilsonjvs <janilsonjvs@gmail.com>
2023-01-17 12:26:53 -0300

    feat: form request update estado

---
+++ app/Http/Requests/EstadoUpdateFormRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Estado;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EstadoUpdateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'uf' => [function($attribute, $value, $fail){
+                if ($value == "") {
+                    $fail("O :attribute não pode ser vazio.");
+                }
+            },
+            function($attribute, $value, $fail){
+                if ($value != "") {
+                    $reg = Estado::where($attribute, $value)
+                        ->where('estados.id', '<>', $this->route('estado'))
+                        ->first();
+                    if (isset($reg)) {
+                        $fail("A :attribute já cadastrado.");
+                    }
+                }
+            }
+            ],
+            'nome' => [function($attribute, $value, $fail) {
+                if ($value == "") {
+                    $fail("O :attribute não pode ser vazio.");
+                }
+            }]
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'uf' => 'UF',
+            'nome' => 'Nome'
+        ];
+    }
+}
