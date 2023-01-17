cfb40382c1193b10309ca4fed2b8456a5676ccc8
janilsonjvs <janilsonjvs@gmail.com>
2023-01-17 09:35:53 -0300

    feat: model conta

---
+++ app/Models/Conta.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conta extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'numero_banco',
+        'numero_agencia',
+        'numero_conta',
+        'tipo_conta',
+        'data_abertura',
+        'saldo',
+        'ativo'
+    ];
+}
