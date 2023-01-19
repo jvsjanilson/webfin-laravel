98d482965d4772a6e0b2480c0151d799da81e64c
janilsonjvs <janilsonjvs@gmail.com>
2023-01-19 08:20:44 -0300

    feat: model conta Receber

---
+++ app/Models/ContaReceber.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContaReceber extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'documento',
+        'emissao',
+        'vencimento',
+        'valor',
+        'desconto',
+        'juros',
+        'multa',
+        'data_pagamento',
+        'conta_id',
+        'cliente_id',
+    ];
+}
