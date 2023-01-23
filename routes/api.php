bc204db6ff11f1e4b5303c02dbf933c45e26e3f1
janilsonjvs <janilsonjvs@gmail.com>
2023-01-23 10:07:41 -0300

    feat: rotas conta a pagar

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CidadeController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ContaController;
+use App\Http\Controllers\ContaPagarController;
 use App\Http\Controllers\ContaReceberController;
 use App\Http\Controllers\EstadoController;
 use App\Http\Controllers\FornecedorController;
@@ -26,11 +27,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware(['auth:sanctum'])->group(function () {
+// Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/estados', EstadoController::class);
     Route::apiResource('/cidades', CidadeController::class);
     Route::apiResource('/clientes', ClienteController::class);
     Route::apiResource('/fornecedores', FornecedorController::class);
     Route::apiResource('/contas', ContaController::class);
     Route::apiResource('/contarecebers', ContaReceberController::class);
-});
+    Route::apiResource('/contapagars', ContaPagarController::class);
+// });
