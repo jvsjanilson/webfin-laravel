4033fd6b7ec8e8e3989a301ec630174997568490
janilsonjvs <janilsonjvs@gmail.com>
2023-01-23 10:07:06 -0300

    feat: resource e collection conta pagar

---
+++ app/Http/Resources/ContaPagarResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContaPagarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'documento'     => $this->documento,
+            'emissao'       => $this->emissao,
+            'vencimento'    => $this->vencimento,
+            'valor'         => $this->valor,
+            'desconto'      => $this->desconto,
+            'juros'         => $this->juros,
+            'multa'         => $this->multa,
+            'data_pagamento'=> $this->data_pagamento,
+            'conta_id'      => $this->conta_id,
+            'fornecedor_id' => $this->fornecedor_id,
+        ];
+    }
+}
