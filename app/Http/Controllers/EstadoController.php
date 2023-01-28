9d04efc5a8dd963592a391a9b600dbe6ea90f88d
janilsonjvs <janilsonjvs@gmail.com>
2023-01-28 10:40:46 -0300

    feat: injetando a interface repository estado no controller

--- app/Http/Controllers/EstadoController.php
+++ app/Http/Controllers/EstadoController.php
@@ -2,112 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\ExceptionErrorCreate;
-use App\Exceptions\ExceptionErrorDestroy;
-use App\Exceptions\ExceptionErrorUpdate;
-use App\Exceptions\ExceptionNotFound;
+use App\Contracts\EstadoInterface;
 use App\Http\Requests\EstadoFormRequest;
 use App\Http\Requests\EstadoUpdateFormRequest;
 use App\Http\Resources\EstadoCollection;
 use App\Http\Resources\EstadoResource;
-use App\Models\Estado;
-use Illuminate\Http\Request;
+
 use Symfony\Component\HttpFoundation\Response;
 
 class EstadoController extends Controller
 {
+
     private $model;
 
-    public function __construct(Estado $model)
+    public function __construct(EstadoInterface $model)
     {
         $this->model = $model;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $regs = $this->model->paginate(config('app.paginate'));
+        $regs = $this->model->findAll();
         return new EstadoCollection($regs);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(EstadoFormRequest $request)
     {
-        $data = $request->all();
-        try {
-            $this->model->create($data);
-            return response('', Response::HTTP_CREATED);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorCreate();
-        }
+        $this->model->store($request->all());
+        return response('', Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $reg = $this->model->find($id);
-
-        if (!isset($reg))
-            throw new ExceptionNotFound();
-
+        $reg = $this->model->findOne($id);
         return response()->json(new EstadoResource($reg));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(EstadoUpdateFormRequest $request, $id)
     {
-        $reg = $this->model->find($id);
-        $data = $request->all();
-
-        if (!isset($reg)) {
-            throw new ExceptionNotFound();
-        }
-
-        if (count($data) == 0) {
-            throw new ExceptionErrorUpdate("Formado de dados passado invalido(s).");
-        }
-
-        try {
-            $reg->update($data);
-            return response()->json(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorUpdate();
-        }
+        $this->model->update($request->all(), $id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        try {
-            $this->model->destroy($id);
-            return response(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorDestroy();
-        }
+        $this->model->destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
