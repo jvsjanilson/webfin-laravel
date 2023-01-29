4cebe897ec3e3a8ae2a99618075f58ae26a64cbb
janilsonjvs <janilsonjvs@gmail.com>
2023-01-29 11:00:23 -0300

    feat: implentando inteface no controller

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,114 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\ExceptionErrorCreate;
-use App\Exceptions\ExceptionErrorDestroy;
-use App\Exceptions\ExceptionErrorUpdate;
-use App\Exceptions\ExceptionNotFound;
+use App\Contracts\IFornecedor;
 use App\Http\Requests\FornecedorFormRequest;
 use App\Http\Requests\FornecedorUpdateFormRequest;
 use App\Http\Resources\FornecedorCollection;
 use App\Http\Resources\FornecedorResource;
-use App\Models\Fornecedor;
-use Exception;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FornecedorController extends Controller
 {
     private $model;
 
-    public function __construct(Fornecedor $model)
+    public function __construct(IFornecedor $model)
     {
         $this->model = $model;
     }
 
-   /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $regs = $this->model->paginate(config('app.paginate'));
-        return new FornecedorCollection($regs);
+        return new FornecedorCollection($this->model->findAll());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(FornecedorFormRequest $request)
     {
-        $data = $request->all();
-        try {
-            $this->model->create($data);
-            return response('', Response::HTTP_CREATED);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorCreate();
-        }
+        $this->model->store($request->all());
+        return response('', Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $reg = $this->model->find($id);
-        if (!isset($reg))
-            throw new ExceptionNotFound();
-
-        return response()->json(new FornecedorResource($reg));
+        return response()->json(new FornecedorResource($this->model->findOne($id)));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(FornecedorUpdateFormRequest $request, $id)
     {
-        $reg = $this->model->find($id);
-        $data = $request->all();
-
-
-
-        if (!isset($reg)) {
-            throw new ExceptionNotFound();
-        }
-
-        if (count($data) == 0) {
-            throw new ExceptionErrorUpdate("Formado de dados passado invalido(s).");
-        }
-
-        try {
-            $reg->update($data);
-            return response()->json(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorUpdate();
-        }
+        $this->model->update($request->all(), $id);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        try {
-            $this->model->find($id)->delete();
-            return response(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorDestroy();
-        }
+        $this->model->destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
