cfc51902aceef017e6183f04ce8586e23fdf9977
janilsonjvs <janilsonjvs@gmail.com>
2023-01-29 10:53:44 -0300

    feat: implementando  a interface icliente no controller

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -2,110 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\ExceptionErrorCreate;
-use App\Exceptions\ExceptionErrorDestroy;
-use App\Exceptions\ExceptionErrorUpdate;
-use App\Exceptions\ExceptionNotFound;
+use App\Contracts\ICliente;
 use App\Http\Requests\ClienteFormRequest;
 use App\Http\Requests\ClienteUpdateFormRequest;
 use App\Http\Resources\ClienteCollection;
 use App\Http\Resources\ClienteResource;
-use App\Models\Cliente;
 use Symfony\Component\HttpFoundation\Response;
 
 class ClienteController extends Controller
 {
     private $model;
 
-    public function __construct(Cliente $model)
+    public function __construct(ICliente $model)
     {
         $this->model = $model;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $regs = $this->model->paginate(config('app.paginate'));
-        return new ClienteCollection($regs);
+        return new ClienteCollection($this->model->findAll());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(ClienteFormRequest $request)
     {
-        $data = $request->all();
-        try {
-            $this->model->create($data);
-            return response('', Response::HTTP_CREATED);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorCreate();
-        }
+        $this->model->store($request->all());
+        return response('', Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $reg = $this->model->find($id);
-        if (!isset($reg))
-            throw new ExceptionNotFound();
-
-        return response()->json(new ClienteResource($reg));
+        return response()->json(new ClienteResource($this->model->findOne($id)));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(ClienteUpdateFormRequest $request, $id)
     {
-        $reg = $this->model->find($id);
-        $data = $request->all();
-
-        if (!isset($reg)) {
-            throw new ExceptionNotFound();
-        }
-
-        if (count($data) == 0) {
-            throw new ExceptionErrorUpdate("Formado de dados passado invalido(s).");
-        }
-
-        try {
-            $reg->update($data);
-            return response()->json(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorUpdate();
-        }
+        $this->model->update($request->all(), $id);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        try {
-            $this->model->find($id)->delete();
-            return response(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorDestroy();
-        }
+        $this->model->destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
