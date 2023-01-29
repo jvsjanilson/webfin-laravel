bbc432e7aeec552d8c2523763cf2990ce0a7f06f
janilsonjvs <janilsonjvs@gmail.com>
2023-01-29 10:41:00 -0300

    feat: refatorando controller conta para usar a interface iconta

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -2,114 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Exceptions\ExceptionErrorCreate;
-use App\Exceptions\ExceptionErrorDestroy;
-use App\Exceptions\ExceptionErrorUpdate;
-use App\Exceptions\ExceptionNotFound;
+use App\Contracts\IConta;
 use App\Http\Requests\ContaFormRequest;
 use App\Http\Requests\ContaUpdateFormRequest;
 use App\Http\Resources\ContaCollection;
 use App\Http\Resources\ContaResource;
-use App\Models\Conta;
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ContaController extends Controller
 {
-
     private $model;
 
-    public function __construct(Conta $model)
+    public function __construct(IConta $model)
     {
         $this->model = $model;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $regs = $this->model->paginate(config('app.paginate'));
-        return new ContaCollection($regs);
+        return new ContaCollection($this->model->findAll());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(ContaFormRequest $request)
     {
-        $data = $request->all();
-
-        try {
-            $this->model->create($data);
-            return response('', Response::HTTP_CREATED);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorCreate();
-        }
+        $this->model->store($request->all());
+        return response('', Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $reg = $this->model->find($id);
-
-        if (!isset($reg))
-            throw new ExceptionNotFound();
-
-        return response()->json(new ContaResource($reg));
+        return response()->json(new ContaResource($this->model->findOne($id)));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(ContaUpdateFormRequest $request, $id)
     {
-        $reg = $this->model->find($id);
-        $data = $request->all();
-
-        if (!isset($reg)) {
-            throw new ExceptionNotFound();
-        }
-
-        if (count($data) == 0) {
-            throw new ExceptionErrorUpdate("Formado de dados passado invalido(s).");
-        }
-
-        try {
-            $reg->update($data);
-            return response()->json(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorUpdate();
-        }
+        $this->model->update($request->all(), $id);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        try {
-            $this->model->find($id)->delete();
-            return response(null, Response::HTTP_NO_CONTENT);
-        } catch (\Throwable $th) {
-            throw new ExceptionErrorDestroy();
-        }
+        $this->model->destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
