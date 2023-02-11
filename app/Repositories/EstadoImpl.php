28bfc163fe5962a6bb3876c60f7c73c9724fab39
janilsonjvs <janilsonjvs@gmail.com>
2023-02-11 11:21:49 -0300

    feat: method all para carregar todos os estados

--- app/Repositories/EstadoImpl.php
+++ app/Repositories/EstadoImpl.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use App\Contracts\IEstado;
 use App\Models\Estado;
-
+use App\Exceptions\ExceptionNotFound;
 
 class EstadoImpl extends AbstractRepos implements IEstado
 {
@@ -13,5 +13,41 @@ class EstadoImpl extends AbstractRepos implements IEstado
     public function __construct(Estado $model)
     {
         $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function findAll()
+    {
+        $search = request()->nome;
+
+        $regs =  $this->model
+            ->when($search != "", function($q) use ($search) {
+                $q->where(function($query) use ($search) {
+                    $query->where('nome', 'like', '%'. $search.'%');
+                    $query->orWhere('uf', 'like', '%'. $search.'%');
+                });
+            })
+            ->orderBy('id', 'desc')->paginate(config('app.paginate'));
+        return $regs;
+    }
+
+    public function findByUF($uf)
+    {
+        $id = request()->id;
+
+        $reg = $this->model->where('uf', $uf)
+            ->when(isset($id), function($q) use ($id) {
+                $q->where(function($query) use ($id) {
+                    $query->where('id', '<>', $id);
+                });
+            })
+            ->first();
+         if (!isset($reg))
+             throw new ExceptionNotFound();
+        return $reg;
     }
 }
