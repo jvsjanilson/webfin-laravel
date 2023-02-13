84d34a43bc77e9edf5f42352dc50dd0d2888983c
janilsonjvs <janilsonjvs@gmail.com>
2023-02-13 19:07:42 -0300

    feat: ajustando find all no repository cliente

--- app/Repositories/ClienteImpl.php
+++ app/Repositories/ClienteImpl.php
@@ -13,4 +13,20 @@ class ClienteImpl extends AbstractRepos implements ICliente
     {
         $this->model = $model;
     }
+
+    public function findAll()
+    {
+        $search = request()->nome;
+
+        $regs =  $this->model
+            ->when($search != "", function($q) use ($search) {
+                $q->where(function($query) use ($search) {
+                    $query->where('nome', 'like', '%'. $search.'%');
+                    $query->orWhere('cpfcnpj', 'like', '%'. $search.'%');
+                    $query->orWhere('celular', 'like', '%'. $search.'%');
+                });
+            })
+            ->orderBy('id', 'desc')->paginate(config('app.paginate'));
+        return $regs;
+    }
 }
